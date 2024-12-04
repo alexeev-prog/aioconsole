e8c680f408d9a732964fa68afb89479421e9b8f8
alexeev-prog <alexeev.dev@mail.ru>
2024-12-04 20:53:30 +0700

    feat: add simple application architecture

---
+++ docs/en/index.md
@@ -0,0 +1,1 @@
+# AIOConsole Docs
--- example.py
+++ example.py
@@ -1,18 +1,32 @@
 import asyncio
-from pyaioconsole.app import Application, Settings
+
+from pyaioconsole.app import Application
+from pyaioconsole.app import Settings
 
 settings = Settings(
-	APP_NAME='Example App',
-	BRIEF='Short brief description',
-	LONG_DESC='aioconsole library example application'
+	APP_NAME="Example App",
+	BRIEF="Short brief description",
+	LONG_DESC="aioconsole library example application",
 )
 
 app = Application(settings)
 
 
+@app.command(help="Say hello")
+@app.argument("name", help="name")
+async def hello(name: str):
+	print(f"Hello, {name}!")
+
+
+@app.command(help="Say bye")
+@app.argument("name", help="name")
+async def bye(name: str):
+	print(f"Bye, {name}!")
+
+
 async def main():
-	await app.help()
+	await app.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	asyncio.run(main())
